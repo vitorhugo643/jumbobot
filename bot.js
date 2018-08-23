a3ec3659f48bd86b40012418ad0bea9b6440cd07
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-08-23 18:12:59 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,6 @@ const bot = new Discord.Client();
 bot.on('ready', () 
     console.log('Logado');
 });
-bot.on('message', message => {
-    if (message.content === 'Oi') {
-    	message.reply('Olá, eu sou i filho do jumbo<3!');
-    }
-});
 
 // THIS  MUST  BE  THIS  WAY
 bot.login(process.env.BOT_TOKEN);
