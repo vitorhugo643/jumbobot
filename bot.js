46cf3e49e9135988b772b397dc77e799528d25f3
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-08-23 18:10:45 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,17 +5,10 @@ const bot = new Discord.Client();
 bot.on('ready', () 
     console.log('Logado');
 });
-bot.on('guildMemberAdd', member => {
-  const randomColor = "#000000".replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16); });
- 
-  let canal = member.guild.channels.find(`name`, "📥entrada");
-  if (!canal) return;
-
-  var embed = new Discord.RichEmbed()
-  .setColor(randomColor)
-  .setDescription(`🎈 **Olá ${member}, seja bem-vindo ao Discord oficial do JumboBR**`)
-  .setThumbnail(member.user.displayAvatarURL)
-  canal.send({embed : embed})
+bot.on('message', message => {
+    if (message.content === 'Oi') {
+    	message.reply('Olá, eu sou i filho do jumbo<3!');
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
